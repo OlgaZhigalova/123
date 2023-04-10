c42a2ef9ffc4cc167a05891424d394b1c07dc08b
OlgaZhigalova <130130121+OlgaZhigalova@users.noreply.github.com>
2023-04-10 10:41:24 +0300

    Add files via upload

---
+++ localhost/Lab1/Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html
+<html>
+<head>
+<title> Moя страничка </title> <link rel="stylesheet" href="mycss.css">
+<link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo "периметр прямоугольника со сторонами 7 и 9 равен ";
+echo (7 + 9) * 2;
+?>
+</div>
+</body>
+</html>
